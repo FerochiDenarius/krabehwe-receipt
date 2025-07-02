59b6c1e5708b83c1f9e1c5a631cf1f588bdc3004
FerochiDenarius <ofosumenyabrightkofi@gmail.com>
2025-07-02 22:48:18 +0000

    Started setup Springboot project structure project architecture

---
+++ KrabehweReceipt/src/main/java/com/krabehwe/receipt/model/StoreInfo.java
@@ -0,0 +1,28 @@
+package com.krabehwe.receipt.model;
+
+import java.awt.image.BufferedImage;
+
+public class StoreInfo {
+
+    public static void main (String[] args)
+
+    {
+
+        String storeName ;
+        String storeAddress;
+        String phoneNo;
+        String taxRegNo;
+        String email;
+        String whatsappLine;
+        BufferedImage storeLogo;
+
+
+
+
+    }
+
+
+
+
+
+}
